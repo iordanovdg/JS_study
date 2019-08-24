3e285f62be2f729c6af499a52ad6346fa624306c
Iordanov Dmitriy <iordanov.d.g@yandex.ru>
2019-08-24 10:12:06 +0300

    выполнил урок 1

---
+++ lesson01/main.js
@@ -0,0 +1,7 @@
+let money = 1000,
+	income = 'поступления',
+	addExpenses = 'внесено',
+	deposit = 'депозит',
+	mission = 'цель',
+	period = 'период';
+console.log(addExpenses + ' ' + money + ' ' + 'рублей на' + deposit);
