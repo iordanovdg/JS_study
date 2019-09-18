8874dbdcbc4073c158e1adc264a75d237306f81b
Iordanov Dmitriy <iordanov.d.g@yandex.ru>
2019-09-18 20:32:10 +0300

    14 урок

--- lesson14/main.js
+++ lesson14/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 	'use strict';
 
 	// Timer
@@ -50,7 +50,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	countTimer('31 december 2019');
-	
+
 
 	// Menu
 	const toggleMenu = () => {
@@ -69,41 +69,46 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			// Var 2
 			menu.classList.toggle('active-menu');
-
 		};	
+		
+		document.addEventListener('click', (event) => {
+			let target = event.target,
+			menuBtn = target.closest('.menu'),
+			menuClose = target.closest('.active-menu');
 
-		btnMenu.addEventListener('click', handlerMenu);
-
-		closeBtn.addEventListener('click', handlerMenu);
-
-		menuItems.forEach((elem) => {
-			elem.addEventListener('click', handlerMenu);
+			if(menuBtn){
+				handlerMenu();
+			} else if (target.classList.contains('close-btn') || target.matches('li > a') || !menuClose) {
+				menu.classList.remove('active-menu');
+			
+			} 
 		});
-	
 	};
-
+	
 	toggleMenu();
+
+
 
 	// popup
 	const togglePopUp = () => {
 		const popup = document.querySelector('.popup'),
-		popupBtn = document.querySelectorAll('.popup-btn'),
-		popUpClose = document.querySelector('.popup-close');
+			popupBtn = document.querySelectorAll('.popup-btn');
+			
 		let count = 0,
-				modal,
-				popupContent = document.querySelector('.popup-content');
+			modal,
+			popupContent = document.querySelector('.popup-content');
 
 		let getWidth = window.onresize = () => {
 			let width = window.innerWidth;
 			return width;
-				};
+		};
 
-				
+
 		let popupAnimate = () => {
-		let width =	getWidth();
+			let width = getWidth();
 			modal = requestAnimationFrame(popupAnimate);
 			count++;
-			if( width < 767){
+			if (width < 767) {
 				modal = false;
 			} else if (count < 50) {
 				popupContent.style.top = count * 3 + 'px';
@@ -112,7 +117,7 @@ window.addEventListener('DOMContentLoaded', () => {
 				count = 0;
 			}
 		};
-		
+
 		popupBtn.forEach((elem) => {
 			elem.addEventListener('click', () => {
 				popup.style.display = 'block';
@@ -120,13 +125,68 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		});
 
-		popUpClose.addEventListener(`click`, () => {
-			popup.style.display = 'none';
+		
+
+
+		popup.addEventListener('click', (event) => {
+
+			let target = event.target;
+
+			if (target.classList.contains('popup-close')) {
+				popup.style.display = 'none';
+			} else {
+				target = target.closest('.popup-content');
+
+				if (!target) {
+					popup.style.display = 'none';
+				}
+			}
+
+
+
 		});
 
 	};
 
 	togglePopUp();
-	
 
+
+
+
+	// Табы
+	const tabs = () => {
+
+		const tabHeader = document.querySelector('.service-header'),
+			tab = tabHeader.querySelectorAll('.service-header-tab'),
+			tabContent = document.querySelectorAll('.service-tab');
+
+		const toggleTabContent = (index) => {
+			for (let i = 0; i < tabContent.length; i++) {
+				if (index === i) {
+					tab[i].classList.add('active');
+					tabContent[i].classList.remove('d-none');
+
+				} else {
+					tabContent[i].classList.add('d-none');
+					tab[i].classList.remove('active');
+				}
+			}
+		};
+		tabHeader.addEventListener('click', (event) => {
+			let target = event.target;
+
+			target = target.closest('.service-header-tab');
+
+			if (target) {
+				tab.forEach((item, i) => {
+					if (item === target) {
+						toggleTabContent(i);
+					}
+				});
+			}
+		});
+
+	};
+
+	tabs();
 });
