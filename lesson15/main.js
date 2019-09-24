506104a4a5978d7a064489d6ac19faf78ac58afd
Iordanov Dmitriy <iordanov.d.g@yandex.ru>
2019-09-25 00:47:58 +0300

    домашка

--- lesson15/main.js
+++ lesson15/main.js
@@ -196,22 +196,22 @@ document.addEventListener('DOMContentLoaded', () => {
 	const slider = () => {
 		const slide = document.querySelectorAll('.portfolio-item'),
 			btn = document.querySelectorAll('.portfolio-btn'),
-			dotWrap = document.querySelector('.portfolio-dots'),
+			dotWrap = document.querySelector('.portfolio-dots'),		
 			slider = document.querySelector('.portfolio-content');
-
+	
 		// Добавляем дотсы по количеству слайдов
 		const addDots = () => {
-			for (let i = 0; i < slide.length; i++) {
-				let newDot = document.createElement("li");
-				newDot.classList.add('dot');
-				dotWrap.appendChild(newDot);
-			}
+			for(let i = 0; i < slide.length; i++) {
+			let	newDot = document.createElement("li");
+			newDot.classList.add('dot');		
+			dotWrap.appendChild(newDot);
+			}			
 		};
 		addDots();
 
 		let dot = document.querySelectorAll('.dot');
 		dot[0].classList.add('dot-active');
-
+		
 
 		let currentSlide = 0,
 			interval;
@@ -298,26 +298,88 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const img = () => {
 		let comand = document.querySelector('.command');
-
+	
 		comand.addEventListener('mouseover', (event) => {
+			
+			let target = event.target;	
 
-			let target = event.target;
-
-
-			target.src = target.dataset.img;
-
-		});
+			target.dataset.src = target.src;			
+			target.src = target.dataset.img;	
+		
+		});		
+		
 		comand.addEventListener('mouseout', (event) => {
-			let target = event.target;
-			let imgSrc = target.getAttribute('src');
-			console.log(imgSrc);
-			target.src = target.src;
+			let target = event.target;			
+			
+			target.src = target.dataset.src;		
 
 		});
 
 	};
 
-	img();
+ img();
 
-	
+ // калькулятор
+
+ const calc = (price = 100) => {
+
+	const calcBlock = document.querySelector('.calc-block'),
+		calcType = document.querySelector('.calc-type'),
+		calcSquare = document.querySelector('.calc-square'),
+		calcDay = document.querySelector('.calc-day'),
+		calcCount = document.querySelector('.calc-count'),
+		totalValue = document.getElementById('total'),
+		calcBlockInput = calcBlock.querySelectorAll('input');
+
+	const countSum = () => {
+		let total = 0,
+			countValue = 1,
+			dayValue = 1;
+		const typeValue = calcType.options[calcType.selectedIndex].value,
+			squareValue = +calcSquare.value;
+
+			if(calcCount.value > 1){
+				countValue += (calcCount.value - 1) / 10
+			}
+
+			if(calcDay.value && calcDay.value < 5) {
+				dayValue *=2;
+			} else if (calcDay.value && calcDay.value < 10){
+				dayValue *=1.5;
+			}
+
+
+			if(typeValue && squareValue) {
+				total = price * typeValue * squareValue * countValue * dayValue;
+			} 
+
+
+		totalValue.textContent = total;
+	};
+
+	calcBlock.addEventListener('change', (event) => {
+		let target = event.target;
+
+		// var1
+
+		// if(target === calcDay || target ===  calcType || target === calcSquare || target ===  calcCount) {
+		// 	console.log(1);
+		// }
+
+		// var 2
+		if (target.matches('select') || target.matches('input')) {
+
+			calcBlockInput.forEach((elem) => {
+				// console.log(elem);
+				elem.value = elem.value.replace(/\D/g, '');
+				console.log(elem.value);
+			});
+			countSum();
+		}
+	});
+
+};
+
+calc(100);
+
 });
